1784c4723c813a77bd0f3ec0bdbd00e1f304fa9a
Mumtaz <fatim25m@mtholyoke.edu>
2023-11-26 17:51:37 -0500

    Remove index.css and index.jsx files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from './App.jsx'
-import './index.css'
+import './main.css'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
